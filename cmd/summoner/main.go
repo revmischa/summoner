26f97de426e2159cef64447759d5dcfe92094608
Mischa S <revmischa@cpan.org>
2015-07-11 23:44:28 -0700

    creating summon endpoint, slack cmd parsing/response

--- cmd/summoner/main.go
+++ cmd/summoner/main.go
@@ -3,7 +3,9 @@ package main
 import (
     "os"
     "log"
+    "encoding/json"
     "bitbucket.org/ckvist/twilio/twiml"
+    "bitbucket.org/ckvist/twilio/twirest"
     "net/http"
 )
 
@@ -35,5 +37,66 @@ func main() {
     }
 
     http.HandleFunc("/", helloMonkey)
+    http.HandleFunc("/summon", Summon)
     http.ListenAndServe(":" + port, nil)
+}
+
+func SlackReply(w http.ResponseWriter, msg string) {
+    type SlackResponse struct {
+        text string
+    }
+
+    if msg == "" {
+        return
+    }
+
+    res := SlackResponse{ text: msg }
+
+    b, err := json.Marshal(res)
+    if err != nil {
+        log.Fatal("Error encoding slack response: " + err.Error())
+        w.Write([]byte("{'text': 'error'}"))
+        return
+    }
+
+    w.Write([]byte(b))
+}
+
+//  INITIATION
+//////////////
+
+func Summon(w http.ResponseWriter, r *http.Request) {
+    target := r.FormValue("text")
+    from := r.FormValue("user_name")
+
+    if target == "" {
+        log.Fatal("got request with no text")
+        SlackReply(w, "Error: no destination specified")
+        return
+    }
+
+    num := "+1" + target
+    Initiate(num, from)
+    SlackReply(w, "Summoning " + num)
+}
+
+func Initiate(num string, fromName string) {
+    accountSid := os.Getenv("ACCT_SID")
+    authToken := os.Getenv("AUTH_TOKEN")
+    fromNum := os.Getenv("FROM_NUM")
+
+    client := twirest.NewClient(accountSid, authToken)
+
+    msg := twirest.SendMessage{
+            Text: "You have been summoned by " + fromName,
+            To:   num,
+            From: fromNum}
+
+    resp, err := client.Request(msg)
+    if err != nil {
+        log.Fatal(err)
+        return
+    }
+
+    log.Print(resp.Message.Status)
 }
